94f8d4825aba4796a7ebd05c0a2569c97c6c30ab
hamehada <58239790+hamehada@users.noreply.github.com>
2024-06-03 10:45:22 +0700

    Update mobile.js

--- routes/mobile.js
+++ routes/mobile.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { authenticateToken } = require('../middleware/auth_user');
 const authMiddleware = require('../middleware/authMiddelware');
 module.exports = function (app) {
     let mobileUser = require("../controller/mobile");
@@ -8,8 +9,24 @@ module.exports = function (app) {
     .post(mobileUser.userController.register);
 
     app.route(`/api/user/login/`)
-    .get(mobileUser.userController.login);
+    .post(mobileUser.userController.login);
 
     app.route(`/api/user/editUser`)
     .put(mobileUser.userController.getUser);
+
+    app.route(`/api/user/getWisata`)
+    .get(mobileUser.account_controller.getWisata)
+
+    app.route(`/api/user/getkendaraan/:id`)
+    .get(mobileUser.account_controller.getkendaraan)
+
+    app.route(`/api/user/getHotel/:id`)
+    .get(mobileUser.account_controller.getHotel)
+
+    app.route(`/api/user/getPaketwisata/`)
+    .get(mobileUser.account_controller.getPaketwisata)
+
+    app.route(`/api/user/datadiri/`)
+    .get(authenticateToken,mobileUser.account_controller.datadiri)
+
 }
